f0538b124b8ca62593eba8fa992cbf890cdfaa7b
Tyler Hawkins <thawkin3@byu.net>
2025-09-27 23:34:28 -0600

    feat: adds buttons and images

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,53 @@
-import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
+    <main>
+      <h1>Fantastic Assistive Technologies and Where to Find Them</h1>
+      <h2>Keyboard-only navigation</h2>
+      <div className="hStack">
+        <button
+          className="btn primary"
+          type="button"
+          onClick={() => alert('clicked button 1!')}
+        >
+          Button 1
+        </button>
+        <div className="btn primary" onClick={() => alert('clicked button 2!')}>
+          Button 2
+        </div>
+        <div
+          className="btn primary"
+          role="button"
+          tabIndex={0}
+          onClick={() => alert('clicked button 3!')}
+          onKeyPress={(event) => {
+            if (event.key === ' ' || event.key === 'Enter') {
+              alert('clicked button 3!');
+            }
+          }}
+        >
+          Button 3
+        </div>
+        <button
+          className="btn primary poorFocus"
+          type="button"
+          onClick={() => alert('clicked button 4!')}
+        >
+          Button 4
+        </button>
+        <button
+          className="btn primary"
+          type="button"
+          onClick={() => alert('clicked button 5!')}
+        >
+          Button 5
+        </button>
+      </div>
+      <h2>Images</h2>
+      <div className="hStack">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,19 +55,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   );
 }
 
