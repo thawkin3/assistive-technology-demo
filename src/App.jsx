ab876fc14c2e6cf49a596e27295d4cb1178edc22
Tyler Hawkins <thawkin3@byu.net>
2025-09-28 08:57:49 -0600

    refactor: runs prettier

--- src/App.jsx
+++ src/App.jsx
@@ -47,6 +47,13 @@ function App() {
         >
           Button 5
         </a>
+        <button
+          className="btn primary"
+          type="button"
+          onClick={() => alert('clicked button 6!')}
+        >
+          Button 6
+        </button>
       </div>
       <h2>Images</h2>
       <div className="hStack">
@@ -58,10 +65,42 @@ function App() {
         </a>
       </div>
       <h2>Text</h2>
-        <p className="adjustableFontSize"><b>This text will adjust to browser zoom and text zoom.</b></p>
-        <p className="adjustableFontSize">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis justo et nisl efficitur tincidunt. Aliquam erat volutpat. Etiam at molestie velit. Nunc lobortis diam quis velit aliquet pellentesque. Curabitur laoreet ex eu justo efficitur, vitae sollicitudin risus facilisis. Sed sodales augue vitae sem sollicitudin, vel lobortis justo scelerisque. Nunc malesuada ligula id nibh elementum, ac tincidunt mauris convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam sodales eu nulla a dictum. Donec vitae nibh at diam pellentesque varius quis eu quam. Duis odio felis, varius ut congue scelerisque, pulvinar et quam. Etiam scelerisque dapibus fermentum. Sed quis mattis lorem. Ut aliquam laoreet diam pulvinar efficitur. Ut ac metus rhoncus justo convallis molestie. Morbi et justo at ligula dignissim tincidunt.</p>
-        <p className="nonAdjustableFontSize"><b>This text will adjust to browser zoom but not text zoom.</b></p>
-        <p className="nonAdjustableFontSize">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis justo et nisl efficitur tincidunt. Aliquam erat volutpat. Etiam at molestie velit. Nunc lobortis diam quis velit aliquet pellentesque. Curabitur laoreet ex eu justo efficitur, vitae sollicitudin risus facilisis. Sed sodales augue vitae sem sollicitudin, vel lobortis justo scelerisque. Nunc malesuada ligula id nibh elementum, ac tincidunt mauris convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam sodales eu nulla a dictum. Donec vitae nibh at diam pellentesque varius quis eu quam. Duis odio felis, varius ut congue scelerisque, pulvinar et quam. Etiam scelerisque dapibus fermentum. Sed quis mattis lorem. Ut aliquam laoreet diam pulvinar efficitur. Ut ac metus rhoncus justo convallis molestie. Morbi et justo at ligula dignissim tincidunt.</p>
+      <p className="adjustableFontSize">
+        <b>This text will adjust to browser zoom and text zoom.</b>
+      </p>
+      <p className="adjustableFontSize">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        quis justo et nisl efficitur tincidunt. Aliquam erat volutpat. Etiam at
+        molestie velit. Nunc lobortis diam quis velit aliquet pellentesque.
+        Curabitur laoreet ex eu justo efficitur, vitae sollicitudin risus
+        facilisis. Sed sodales augue vitae sem sollicitudin, vel lobortis justo
+        scelerisque. Nunc malesuada ligula id nibh elementum, ac tincidunt
+        mauris convallis. Orci varius natoque penatibus et magnis dis parturient
+        montes, nascetur ridiculus mus. Etiam sodales eu nulla a dictum. Donec
+        vitae nibh at diam pellentesque varius quis eu quam. Duis odio felis,
+        varius ut congue scelerisque, pulvinar et quam. Etiam scelerisque
+        dapibus fermentum. Sed quis mattis lorem. Ut aliquam laoreet diam
+        pulvinar efficitur. Ut ac metus rhoncus justo convallis molestie. Morbi
+        et justo at ligula dignissim tincidunt.
+      </p>
+      <p className="nonAdjustableFontSize">
+        <b>This text will adjust to browser zoom but not text zoom.</b>
+      </p>
+      <p className="nonAdjustableFontSize">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        quis justo et nisl efficitur tincidunt. Aliquam erat volutpat. Etiam at
+        molestie velit. Nunc lobortis diam quis velit aliquet pellentesque.
+        Curabitur laoreet ex eu justo efficitur, vitae sollicitudin risus
+        facilisis. Sed sodales augue vitae sem sollicitudin, vel lobortis justo
+        scelerisque. Nunc malesuada ligula id nibh elementum, ac tincidunt
+        mauris convallis. Orci varius natoque penatibus et magnis dis parturient
+        montes, nascetur ridiculus mus. Etiam sodales eu nulla a dictum. Donec
+        vitae nibh at diam pellentesque varius quis eu quam. Duis odio felis,
+        varius ut congue scelerisque, pulvinar et quam. Etiam scelerisque
+        dapibus fermentum. Sed quis mattis lorem. Ut aliquam laoreet diam
+        pulvinar efficitur. Ut ac metus rhoncus justo convallis molestie. Morbi
+        et justo at ligula dignissim tincidunt.
+      </p>
     </main>
   );
 }
